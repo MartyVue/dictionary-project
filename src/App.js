5d6fc4295775f38686092a4611a3c0d1c3c272a5
MartyVue <84153688+MartyVue@users.noreply.github.com>
2022-02-06 10:47:04 -0700

    Check bootstrap

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={dictionary} className="App-logo" alt="dictionary" />
+        <a href="#" className="btn btn-info shadow">
+          Search
+        </a>
       </header>
     </div>
   );
