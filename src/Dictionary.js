d0c52b4d6331ad39472d598150a8cac2241b9eea
MartyVue <84153688+MartyVue@users.noreply.github.com>
2022-02-07 07:41:43 -0700

    edited api Url

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,10 +11,10 @@ export default function Dictionary() {
 
     function search(event) {
         event.preventDefault();
-        
+
         alert(`Searching for definition of ${keyword}`);
 
-        let apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en_US/word";
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
     }
 
