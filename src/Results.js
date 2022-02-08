aee9793390fc9c3ead99cfa0af05e66183a48945
MartyVue <84153688+MartyVue@users.noreply.github.com>
2022-02-07 19:07:37 -0700

    added component for results and meaning

--- src/Results.js
+++ src/Results.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Meaning from "./Meaning";
 
 export default function Results(props) {
     if (props.results) {
@@ -6,8 +7,12 @@ export default function Results(props) {
             <div className="Results">
                 <h2>{props.results.wrod}</h2>
                 {props.results.meanings.map(function (meaning, index) {
-                    return meaning.definitions[0].definition;
-                })}
+                    return (
+                        <div key={index}>
+                            <Meaning meaing={meaning} />
+                        </div>
+                    );
+                })}    
             </div>
         );
     } else {
