abe83dee6fd7b1f37755a07fa7f93c4b99120af3
MartyVue <84153688+MartyVue@users.noreply.github.com>
2022-02-13 11:17:28 -0700

    adding css to dictionary

--- src/Results.js
+++ src/Results.js
@@ -6,6 +6,7 @@ export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
+        <section>
         <h2>{props.results.word}</h2>
         {props.results.phonetics.map(function (phonetic, index) {
           return (
@@ -16,11 +17,12 @@ export default function Results(props) {
         })}
         {props.results.meanings.map(function (meaning, index) {
           return (
-            <div key={index}>
+            <section key={index}>
               <Meaning meaning={meaning} />
-            </div>
+            </section>
           );
         })}
+        </section>
       </div>
     );
   } else {
