6ecf2c9770700a0cccd0f8fead776d0d1a566c26
MartyVue <84153688+MartyVue@users.noreply.github.com>
2022-02-13 22:01:36 -0700

    updated default word.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
           <img src={logo} className="App-logo" alt="logo" />
         </header>
       <main>
-        <Dictionary defaultKeyword="sunset" />
+        <Dictionary defaultKeyword="flower" />
       </main>
       <footer className="App-footer">
         <small>
